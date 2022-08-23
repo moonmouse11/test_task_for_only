77c15ed8c357dfb84d0dcdf8651f1df8b9d951ab
moonmouse11 <70607402+moonmouse11@users.noreply.github.com>
2022-08-24 00:00:28 +0500

    Update index.php
    style work

--- practiceWorkForOnly/login/index.php
+++ practiceWorkForOnly/login/index.php
@@ -14,10 +14,9 @@ if (! empty($_POST)) {
         session_start();
         $_SESSION['id'] = checkUser($user)['id'];
     }
-
 }
-
 ?>
+
 <div class="row">
     <div class="box">
         <div class="col-lg-12">
